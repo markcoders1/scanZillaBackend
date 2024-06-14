bf40336d8273e3c5b431e866f0586fef0eba7d5d
Haris <haris.markcoders@gmail.com>
2024-06-14 19:27:15 +0500

    cors error

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin:'https://scan-zilla-frontend.vercel.app/',
+    origin:'https://scan-zilla-frontend.vercel.app',
     credentials:true
   })
 );
