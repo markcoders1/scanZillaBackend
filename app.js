2a6c9c384d0bd34de020db9b4932df2ce8a526b4
Haris <haris.markcoders@gmail.com>
2024-07-24 21:48:28 +0500

    rate limits

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(express.static("public"));
 
 //rate limits
 
-// app.use('/',defaultLimiter)
+app.use('/',defaultLimiter)
 
 // routes declaration
 
--- middleware/limit.middleware.js
+++ middleware/limit.middleware.js
@@ -2,12 +2,12 @@ import { rateLimit } from 'express-rate-limit';
 
 
 export const defaultLimiter = rateLimit({
-	windowMs: 1 * 60 * 1000, // 1 minute
-	limit: 20 , // Limit each IP to 100 requests per `window` (here, per 20 minutes).
-	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-	// store: ... , // Redis, Memcached, etc. See below.
+	windowMs: 1 * 60 * 1000,
+	limit: 30 ,
+	standardHeaders: 'draft-7',
+	legacyHeaders: false,
 
     message: ()=>{
-        return {message: "You can only use 20 requests per Minute"}}
+        return {message: "You can only use 20 requests per Minute"}
+	}
 });
