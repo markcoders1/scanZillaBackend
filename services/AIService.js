f1403aa03766baa449e917f933776748cc0a356c
haris.markcoders@gmail.com <haris.markcoders@gmail.com>
2024-09-11 20:00:46 +0500

    ai error handling

--- services/AIService.js
+++ services/AIService.js
@@ -33,48 +33,54 @@ async function createMessage(thread_id, role, content) {
 //assId = asst_J8gYM42wapsrXpntcCLMe8wJ
 
 export const analyzeValue = async (value,assistant) => {
-    let latest_message;
-    if(assistant == 'title'){
-        assId = 'asst_3nOxuR6z7N3xY1ZC1WKYAIhe'
-    }else if(assistant == 'desc'){
-        assId = 'asst_GokOIlMbjA1jlvKb8pLNMR51'
-    }else if(assistant == 'bullets'){
-        assId = 'asst_vZhSQFlyB4lcTEaJhk0FitZa'
-    }
-    const { thread_id, id } = await openai.beta.threads.createAndRun({assistant_id: assId});
-    console.log("threadIdTitle", thread_id);
-    let threadrun = await openai.beta.threads.runs.retrieve(thread_id, id);
-    
-    while (threadrun.status === "running" ||threadrun.status === "queued" ||threadrun.status === "in_progress") {
-        console.log("waiting for completion");
-        await new Promise((resolve) => setTimeout(resolve, 1000));
-        threadrun = await openai.beta.threads.runs.retrieve(
-            thread_id,
-            threadrun.id
-        );
-        console.log(`threadrun status: ${threadrun.status}`);
-    }
-    
-    const message = await createMessage(thread_id,"user",`${value}`);
-    
-    let run = await createRun(thread_id, assId);
-    console.log(`run created: ${run.id} at ${thread_id}`);
-    
-    while (run.status === "running" ||run.status === "queued" ||run.status === "in_progress") {
-        console.log("waiting for completion");
-        await new Promise((resolve) => setTimeout(resolve, 1000));
-        run = await openai.beta.threads.runs.retrieve(thread_id, run.id);
-        console.log(`run status: ${run.status}`);
-    }
-    
-    console.log(`run completed: ${run.id}`);
+    try{
 
-    console.log(`analyzed ${assistant}`)
+        let latest_message;
+        if(assistant == 'title'){
+            assId = 'asst_3nOxuR6z7N3xY1ZC1WKYAIhe'
+        }else if(assistant == 'desc'){
+            assId = 'asst_GokOIlMbjA1jlvKb8pLNMR51'
+        }else if(assistant == 'bullets'){
+            assId = 'asst_vZhSQFlyB4lcTEaJhk0FitZa'
+        }
+        const { thread_id, id } = await openai.beta.threads.createAndRun({assistant_id: assId});
+        console.log("threadIdTitle", thread_id);
+        let threadrun = await openai.beta.threads.runs.retrieve(thread_id, id);
+        
+        while (threadrun.status === "running" ||threadrun.status === "queued" ||threadrun.status === "in_progress") {
+            // console.log("waiting for completion");
+            await new Promise((resolve) => setTimeout(resolve, 1000));
+            threadrun = await openai.beta.threads.runs.retrieve(
+                thread_id,
+                threadrun.id
+            );
+            // console.log(`threadrun status: ${threadrun.status}`);
+        }
+        
+        const message = await createMessage(thread_id,"user",`${value}`);
+        
+        let run = await createRun(thread_id, assId);
+        console.log(`run created: ${run.id} at ${thread_id}`);
+        
+        while (run.status === "running" ||run.status === "queued" ||run.status === "in_progress") {
+            // console.log("waiting for completion");
+            await new Promise((resolve) => setTimeout(resolve, 1000));
+            run = await openai.beta.threads.runs.retrieve(thread_id, run.id);
+            // console.log(`run status: ${run.status}`);
+        }
+        
+        console.log(`run completed: ${run.id}`);
     
-    const message_response = await openai.beta.threads.messages.list(thread_id);
-    const messages = message_response.data;
+        console.log(`analyzed ${assistant}`)
+        
+        const message_response = await openai.beta.threads.messages.list(thread_id);
+        const messages = message_response.data;
+        
+        latest_message = messages[0]?.content[0]?.text?.value;
     
-    latest_message = messages[0]?.content[0]?.text?.value;
-
-    return JSON.parse(latest_message);
+        return JSON.parse(latest_message);
+    }catch(err){
+        console.log(assistant,err);
+        return {}
+    }
 }
