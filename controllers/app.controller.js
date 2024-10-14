33c7aad4885e9b8c943a55129287902430479240
haris.markcoders@gmail.com <haris.markcoders@gmail.com>
2024-10-14 21:11:39 +0500

    email config

--- controllers/app.controller.js
+++ controllers/app.controller.js
@@ -971,7 +971,7 @@ export const supportEmail = async (req,res) =>{
         console.log(req.user)
 
         transporter.sendMail({
-            to: "haris.markcoders@gmail.com",
+            to: "amz@blazecopywriting.com",
             subject: "support",
             text: `
             
