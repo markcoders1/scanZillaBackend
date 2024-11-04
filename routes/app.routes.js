a09cebb16a7937ccb239e9aa08151bb5dbc208e6
mkdrs <developer@markcoders.com>
2024-11-04 21:25:49 +0500

    bugfix

--- routes/app.routes.js
+++ routes/app.routes.js
@@ -1,44 +1,56 @@
 import { Router } from "express";
 import { verifyJWT } from "../middleware/app.middleware.js";
-import { generateThread, verifyText, getUserHistory, getUser, buyCredits, BuyCreditWebhook, getPurchaseHistory, numberOfAnalysed, getCardInfo, toggleAutoCredit,getGraphData, getOffers, addPaymentMethod, getRules, paymentEmail, getMessage, supportEmail} from "../controllers/app.controller.js";
+import {
+  verifyText,
+  getUserHistory,
+  getUser,
+  buyCredits,
+  BuyCreditWebhook,
+  getPurchaseHistory,
+  numberOfAnalysed,
+  getCardInfo,
+  toggleAutoCredit,
+  getGraphData,
+  getOffers,
+  addPaymentMethod,
+  getRules,
+  paymentEmail,
+  supportEmail,
+} from "../controllers/app.controller.js";
 // import { deleteAssistant, listAssistants,retreiveRun,getMessages } from "../ignore/ai.code.js";
 // import { generateAssistant,getMessages, retreiveRun } from "../ai.code.js";
 
 const router = Router();
 
-router.route("/verifyText").post(verifyJWT, verifyText)
+router.route("/verifyText").post(verifyJWT, verifyText);
 
-router.route("/getUserHistory").get(verifyJWT,getUserHistory),
+router.route("/getUserHistory").get(verifyJWT, getUserHistory), router.route("/buycredits").post(verifyJWT, buyCredits);
 
-router.route('/buycredits').post(verifyJWT,buyCredits)
+router.route("/buycreditwebhook").post(BuyCreditWebhook);
 
-router.route('/buycreditwebhook').post(BuyCreditWebhook)
+router.route("/getpurchasehistory").get(verifyJWT, getPurchaseHistory);
 
-router.route('/getpurchasehistory').get(verifyJWT,getPurchaseHistory)
+router.route("/getAnalysedNum").get(verifyJWT, numberOfAnalysed);
 
-router.route('/getAnalysedNum').get(verifyJWT,numberOfAnalysed)
+router.route("/getcardinfo").get(verifyJWT, getCardInfo);
 
-router.route('/getcardinfo').get(verifyJWT,getCardInfo)
+router.route("/toggleautocredit").get(verifyJWT, toggleAutoCredit);
 
-router.route('/toggleautocredit').get(verifyJWT,toggleAutoCredit)
+router.route("/getHistoryGraph").get(verifyJWT);
 
-router.route('/getHistoryGraph').get(verifyJWT)
+router.route("/getUser").get(verifyJWT, getUser);
 
-router.route('/getUser').get(verifyJWT,getUser)
+router.route("/getgraphdata").get(verifyJWT, getGraphData);
 
-router.route('/getgraphdata').get(verifyJWT,getGraphData)
+router.route("/offers").get(verifyJWT, getOffers);
 
-router.route('/offers').get(verifyJWT, getOffers)
+router.route("/addPaymentMethod").get(verifyJWT, addPaymentMethod);
 
-router.route('/addPaymentMethod').get(verifyJWT, addPaymentMethod)
+router.route("/rules").get(verifyJWT, getRules);
 
-router.route('/rules').get(verifyJWT, getRules)
+router.route("/PaymentEmail").post(verifyJWT, paymentEmail);
 
-router.route('/PaymentEmail').post(verifyJWT,paymentEmail)
-
-router.route('/getlastmessage').get(getMessage)
-
-router.route('/supportEmail').post(verifyJWT,supportEmail)
+router.route("/supportEmail").post(verifyJWT, supportEmail);
 
 // router.route('/temp').get(listAssistants)
 
@@ -49,6 +61,5 @@ router.route('/supportEmail').post(verifyJWT,supportEmail)
 // router.route("/getUserInfo").get(verifyJWT,getUserInfo)
 
 // router.route("/gen").post(generateThread)
-
 
 export default router;
