1a31bd2f2cd3658b8e11652a596ffd63885804e8
mkdrs <haris.markcoders@gmail.com>
2024-11-27 00:48:55 +0500

    name change apu

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin:["*","http://localhost:5173","https://scan-zilla-frontend.vercel.app"],
+    origin:["*","http://localhost:5173","https://scanzilla.netlify.app"],
     credentials:true
   })
 );
